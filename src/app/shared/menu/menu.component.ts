21f74ec648ef452aed74c412de1dae04dcd4c94c
Uriel <urielenrique99@gmail.com>
2021-04-18 14:28:59 -0500

    Se agrego el menu de navegacion

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,16 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+
+interface MenuItem {
+    ruta : string,
+    nombre : string
+}
+
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styles: [
-  ]
+  styles: [`
+    li {
+      cursor : pointer;
+    }
+  `]
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  menuItems: MenuItem[] = [
+    {
+      ruta: '/mapas/fullscreen',
+      nombre : 'FullScreen'
+    },
+    {
+      ruta: '/mapas/zoom-range',
+      nombre : 'Zoom Range'
+    },
+    {
+      ruta: '/mapas/marcadores',
+      nombre : 'Marcadores'
+    },
+    {
+      ruta: '/mapas/propiedades',
+      nombre : 'Propiedades'
+    },
+  ]
 }
